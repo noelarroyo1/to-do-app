e56128abc206b164d365d1843ebbd864e63ffc10
Noel Arroyo <noel.arroyo1@gmail.com>
2017-09-28 19:48:00 -0500

    make delete button

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ function onReady(){
     //create a new input for the checkbox
     let checkbox = document.createElement('input');
 
+    //create delete button
+    var remove = document.createElement('button')
+
     //set the input's type to checkbox
     checkbox.type = "checkbox";
 
@@ -27,9 +30,21 @@ function onReady(){
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    //attach delete button to newLi
+    newLi.appendChild(remove);
+
     //empty the input
     newToDoText.value = '';
   });
+
+addToDoForm.addEventListener('click', (event) => {
+  event.preventDefault();
+    var confirmation = confirm('Are you sure you want to delete this item?');
+    if(confirmation == true){
+      return delete(newLi);
+    }
+  });
+
 }
 
 window.onload = function(){
