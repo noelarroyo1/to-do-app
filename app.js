382af65f9d4f007d62ffbae8c1e92ae74cdb7d17
Noel Arroyo <noel.arroyo1@gmail.com>
2017-10-04 15:10:22 -0500

    change state and UI design

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 function onReady(){
-  const addToDoForm = document.getElementById('addToDoForm');
+  var toDos = [];
+  var addToDoForm = document.getElementById('addToDoForm');
+
+  function createNewToDo(){
+    var newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = '';
+
+    renderTheUI(toDos);
+  }
+
+  function renderTheUI(toDos){
+    var toDoList = document.getElementById('toDoList');
+
+    toDoList.innerHTML = '';
+
+    toDos.forEach(function(toDo){
+      var newLi = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.innerHTML = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI(toDos);
+}
+
+  /*const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
@@ -16,10 +55,10 @@ function onReady(){
     let checkbox = document.createElement('input');
 
     //create delete button
-    var remove = document.createElement('button');
+    let remove = document.createElement('button');
     remove.textContent = 'delete';
     remove.addEventListener('click', (event) => {
-      var confirmation = confirm('Are you sure you want to delete this item?');
+      let confirmation = confirm('Are you sure you want to delete this item?');
       if(!confirmation){
         return;
       }
@@ -43,8 +82,8 @@ function onReady(){
 
     //empty the input
     newToDoText.value = '';
- });
-}
+ });*/
+
 
 window.onload = function(){
   onReady();
